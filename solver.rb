c61e3d2a8009e38fc364b9845ae36f8385021ef0
sambeck87 <sandro_mipb@hotmail.com>
2023-03-16 14:57:46 -0600

    Add reversal method into Solver class Add fizzbuzz method into Solver class

--- solver.rb
+++ solver.rb
@@ -15,4 +15,23 @@ class Solver
       raise NotCorrectNumber, 'The number should be a positive number'
     end
   end
+
+  def self.reverse(word)
+    @word = word
+    @word = @word.reverse
+  end
+
+  def self.fizzbuzz(number)
+    @number = number
+
+    if (@number % 3).zero? && (@number % 5).zero?
+      'fizzbuzz'
+    elsif (@number % 5).zero?
+      'buzz'
+    elsif (@number % 3).zero?
+      'fizz'
+    else
+      @number.to_s
+    end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require_relative '../solver'
 
-describe Solver do
+describe Solver do # rubocop:disable Metrics/BlockLength: Block has too many lines
   context 'When testing Factorial method' do
     it 'When number is 5, factorial should return 120' do
       factorial = Solver.factorial(5)
@@ -18,29 +18,29 @@ describe Solver do
     end
   end
 
-  # context 'When testing reverse method' do
-  #   it 'Reverse method should return "olleH" when it receive "Hello' do
-  #     reverse = Solver.reverse('Hello')
-  #     expect(reverse).to eq('olleH')
-  #   end
-  # end
+  context 'When testing reverse method' do
+    it 'Reverse method should return "olleH" when it receive "Hello' do
+      reverse = Solver.reverse('Hello')
+      expect(reverse).to eq('olleH')
+    end
+  end
 
-  # context 'When testing fizzbuzz method' do
-  #   it 'If the number is divisible by 3 return fizz' do
-  #     fizzbuzz = Solver.fizzbuzz(3)
-  #     expect(fizzbuzz).to eq('fizz')
-  #   end
-  #   it 'If the number is divisible by 5 return buzz' do
-  #     fizzbuzz = Solver.fizzbuzz(5)
-  #     expect(fizzbuzz).to eq('buzz')
-  #   end
-  #   it 'If the number is divisible by 3 and 5 return fizzbuzz' do
-  #     fizzbuzz = Solver.fizzbuzz(15)
-  #     expect(fizzbuzz).to eq('fizzbuzz')
-  #   end
-  #   it 'If the number is not divisible by 3 nor 5 return the number as an string' do
-  #     fizzbuzz = Solver.fizzbuzz(13)
-  #     expect(fizzbuzz).to eq('13')
-  #   end
-  # end
+  context 'When testing fizzbuzz method' do
+    it 'If the number is divisible by 3 return fizz' do
+      fizzbuzz = Solver.fizzbuzz(3)
+      expect(fizzbuzz).to eq('fizz')
+    end
+    it 'If the number is divisible by 5 return buzz' do
+      fizzbuzz = Solver.fizzbuzz(5)
+      expect(fizzbuzz).to eq('buzz')
+    end
+    it 'If the number is divisible by 3 and 5 return fizzbuzz' do
+      fizzbuzz = Solver.fizzbuzz(15)
+      expect(fizzbuzz).to eq('fizzbuzz')
+    end
+    it 'If the number is not divisible by 3 nor 5 return the number as an string' do
+      fizzbuzz = Solver.fizzbuzz(13)
+      expect(fizzbuzz).to eq('13')
+    end
+  end
 end
